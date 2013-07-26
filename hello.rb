6aead8b473e4c8484f27873bfd4650868783ad13
John Scuteri <john.scuteri@gmail.com>
2013-07-25 23:44:24 -0400

    Switching to calculator 1
    I have made the menu of the simple calculator program.

--- hello.rb
+++ hello.rb
@@ -1,2 +1,17 @@
 #!/usr/bin/ruby
-puts "Hello World!"
+
+# Conversion from "Hello World program" into calculator program
+# Written By John Scuteri, Based off a C++ calculator program.
+# Menu
+$answer
+while not %w(Q q).include? $answer do
+# Not working below
+# while (!($answer == 'Q') | !($answer == 'q'))
+	puts "John's Calculator"
+	puts "For addition type \"A\" and hit enter"
+	puts "For subtraction type \"S\" and hit enter"
+	puts "For multiplication type \"M\" and hit enter"
+	puts "For division type \"D\" and hit enter"
+	puts "To quit type \"Q\" and hit enter"
+	$answer = gets.chomp
+end
