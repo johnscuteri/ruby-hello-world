1e491610569bb4ea11f65a3d1871e4419c22fff7
John Scuteri <john.scuteri@gmail.com>
2013-07-28 19:46:37 -0400

    Completed task
    I completed the first task by reogranizing everything into functions

--- hello.rb
+++ hello.rb
@@ -12,19 +12,19 @@ def menu
 	while not %w(Q q).include? answer do
 		# Not working below
 		# while (!(answer == 'Q') | !(answer == 'q'))
-		one = 1 + rand(10)
-		two = 1 + rand(10)
-		puts "John's Math Teacher app"
+		puts "\n"
+		puts "John's Math Tutor Test App"
 		# What it was meant to be from the beginning I forgot what I wanted
-		puts "Type \"A\" and hit enter for addition"
-		puts "Type \"S\" and hit enter for subtraction"
-		puts "Type \"M\" and hit enter for multiplication"
-		puts "Type \"D\" and hit enter for division"
-		puts "Type \"Q\" and hit enter to quit"
+		puts "\n"
+		puts "+ - * / + - * / + - */ + - * / + - * / + - * / + "
+		puts "+ Type \"A\" and hit enter for addition          +"
+		puts "+ Type \"S\" and hit enter for subtraction       +"
+		puts "+ Type \"M\" and hit enter for multiplication    +"
+		puts "+ Type \"D\" and hit enter for division          +"
+		puts "+ Type \"Q\" and hit enter to quit               +"
+		puts "+ - * / + - * / + - */ + - * / + - * / + - * / +"
 		answer = gets.chomp
-		if %w(A a).include? answer
-			add(one, two)
-		end
+		menuBE(answer)
 	end
 end
 
@@ -34,7 +34,7 @@ def add(first, second)
 	puts "   #{second}"
 	puts " +__________"
 	print " = "
-	one = gets.chomp
+	one = gets.chomp.to_i
 	print "\n\n\n"
 	print "#{one} #{two} "
 	if one == two then
@@ -42,6 +42,71 @@ def add(first, second)
 	else
 		puts "Wrong"
 	end
+end
+
+def subt(first, second)
+	two = first - second
+	puts "   #{first}"
+	puts "   #{second}"
+	puts " -__________"
+	print " = "
+	one = gets.chomp.to_i
+	print "\n\n\n"
+	print "#{one} #{two} "
+	if one == two then
+		puts "Correct"
+	else
+		puts "Wrong"
+	end
+end
+
+def mult(first, second)
+	two = first * second
+	puts "   #{first}"
+	puts "   #{second}"
+	puts " *__________"
+	print " = "
+	one = gets.chomp.to_i
+	print "\n\n\n"
+	print "#{one} #{two} "
+	if one == two then
+		puts "Correct"
+	else
+		puts "Wrong"
+	end
+end
+
+def divi(first, second)
+	two = first / second
+	puts "   #{first}"
+	puts " /__________"
+	puts "   #{second}"
+	print "\n = "
+	one = gets.chomp.to_i
+	print "\n\n\n"
+	print "#{one} #{two} "
+	# I need a new if statement(the one below was too "C like"
+	if one == two then
+		puts "Correct"
+	else
+		puts "Wrong"
+	end
+end
+
+def menuBE(choice)
+	one = 1 + rand(10)
+	two = 1 + rand(10)
+	case choice
+	when "A", "a"
+		add(one, two)
+	when "S", "s"
+		subt(one, two)
+	when "M", "m"
+		mult(one, two)
+	when "D", "d"
+		divi(one, two)
+	end
+
 end
 
 menu
