25b3fce99342d10e5f62420fd7c59c8b58772474
John Scuteri <john.scuteri@gmail.com>
2013-07-28 17:38:39 -0400

    Added stuff
    part of add done

--- hello.rb
+++ hello.rb
@@ -5,19 +5,43 @@
 # Menu
 #$answer
 # Ruby Weirdness, the above answer is as unacceptable as a goto in C
+srand
 
 def menu
 	answer = ""
 	while not %w(Q q).include? answer do
 		# Not working below
 		# while (!(answer == 'Q') | !(answer == 'q'))
-		puts "John's Calculator"
+		one = 1 + rand(10)
+		two = 1 + rand(10)
+		puts "John's Math Teacher app"
+		# What it was meant to be from the beginning I forgot what I wanted
 		puts "Type \"A\" and hit enter for addition"
 		puts "Type \"S\" and hit enter for subtraction"
 		puts "Type \"M\" and hit enter for multiplication"
 		puts "Type \"D\" and hit enter for division"
 		puts "Type \"Q\" and hit enter to quit"
 		answer = gets.chomp
+		if %w(A a).include? answer
+			add(one, two)
+		end
 	end
 end
+
+def add(first, second)
+	two = first + second
+	puts "   #{first}"
+	puts "   #{second}"
+	puts " +__________"
+	print " = "
+	one = gets.chomp
+	print "\n\n\n"
+	print "#{one} #{two} "
+	if one == two then
+		puts "Correct"
+	else
+		puts "Wrong"
+	end
+end
+
 menu
